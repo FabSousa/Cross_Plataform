32b65e9d4b967e0cb25e0752c8d133cefd5c37e1
Fábio Sousa <73472338+FabSousa@users.noreply.github.com>
2023-04-25 09:25:19 -0300

    Aula 2504

---
+++ checkpoint02/android/app/src/main/kotlin/com/example/checkpoint02/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.checkpoint02
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ checkpoint02/lib/contato.dart
@@ -0,0 +1,7 @@
+class Contato {
+  String nome;
+  String email;
+  bool isFavorite;
+
+  Contato(this.nome, this.email, {this.isFavorite = false});
+}
