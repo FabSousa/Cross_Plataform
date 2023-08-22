73ca3781e4f054500358bae0af734b3e56f68c39
Fábio Sousa <73472338+FabSousa@users.noreply.github.com>
2023-08-22 08:26:03 -0300

    prof project

--- 2-Semestre/expense_tracker_fiap-3SIF/lib/components/conta_item.dart
+++ 2-Semestre/expense_tracker_fiap-3SIF/lib/components/conta_item.dart
@@ -1,6 +1,7 @@
-import 'package:expense_tracker/models/banco.dart';
-import 'package:expense_tracker/models/conta.dart';
 import 'package:flutter/material.dart';
+
+import '../models/banco.dart';
+import '../models/conta.dart';
 
 class ContaItem extends StatelessWidget {
   final Conta conta;
@@ -8,17 +9,12 @@ class ContaItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final logo = bancosMap[conta.bancoId]?.logo;
     return ListTile(
       leading: CircleAvatar(
-        backgroundImage: Image.asset('images/$logo').image,
+        backgroundImage: AssetImage('images/${bancosMap[conta.bancoId]?.logo}'),
       ),
-      title: Text(conta.id),
-      subtitle: Text(conta.descricao),
-      trailing: Text(
-        conta.tipoConta.name,
-        style: const TextStyle(fontWeight: FontWeight.w500, fontSize: 15),
-      ),
+      title: Text(conta.descricao),
+      subtitle: Text(bancosMap[conta.bancoId]?.nome ?? ''),
     );
   }
 }
--- 2-Semestre/expense_tracker_fiap-3SIF/lib/pages/contas_page.dart
+++ 2-Semestre/expense_tracker_fiap-3SIF/lib/pages/contas_page.dart
@@ -1,6 +1,8 @@
 import 'package:expense_tracker/components/conta_item.dart';
-import 'package:expense_tracker/repository/conta_repository.dart';
+import 'package:expense_tracker/models/conta.dart';
 import 'package:flutter/material.dart';
+
+import '../repository/contas_repositrory.dart';
 
 class ContasPage extends StatefulWidget {
   const ContasPage({super.key});
@@ -10,21 +12,17 @@ class ContasPage extends StatefulWidget {
 }
 
 class _ContasPageState extends State<ContasPage> {
-  final contas = ContaRepository().getContas();
-  
+  final contas = ContasRepository().listarContas();
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: AppBar(
-          title: const Text('Contas'),
-        ),body: ListView.separated(
-          itemCount: contas.length,
-          itemBuilder: (context, index) {
-            final conta = contas[index];
-            return ContaItem(conta: conta);
-          },
-          separatorBuilder: (context, index) {
-            return const Divider();
-          },
-        ));
+    return ListView.separated(
+      itemCount: contas.length,
+      itemBuilder: (context, index) {
+        final conta = contas[index];
+        return ContaItem(conta: conta);
+      },
+      separatorBuilder: (context, index) => const Divider(),
+    );
   }
 }
