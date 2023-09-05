c0a66d9880a3e566821b5b8e05fe372b19cbfe15
Fábio Sousa <73472338+FabSousa@users.noreply.github.com>
2023-09-05 09:52:31 -0300

    Form implementation

--- 2-Semestre/expense_tracker_fiap-3SIF/lib/pages/contas_page.dart
+++ 2-Semestre/expense_tracker_fiap-3SIF/lib/pages/contas_page.dart
@@ -2,7 +2,7 @@ import 'package:expense_tracker/components/conta_item.dart';
 import 'package:expense_tracker/models/conta.dart';
 import 'package:flutter/material.dart';
 
-import '../repository/contas_repositrory.dart';
+import '../repository/contas_repository.dart';
 
 class ContasPage extends StatefulWidget {
   const ContasPage({super.key});
@@ -12,38 +12,41 @@ class ContasPage extends StatefulWidget {
 }
 
 class _ContasPageState extends State<ContasPage> {
-  final contas = ContasRepository().listarContas();
+  final contasFuture = ContasRepository().listarContas();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text("Contas")),
-      body: FutureBuilder<List<Conta>>(
-        future: contas,
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return const Center(child: CircularProgressIndicator());
-          }
-
-          if (snapshot.hasError) {
-            return const Center(child: Text("Erro ao istar as contas"));
-          }
-
-          if (!snapshot.hasData || snapshot.data!.isEmpty) {
-            return const Center(child: Text("Nenhuma conta encontrada"));
-          }
-
-          final contas = snapshot.data!;
-          return ListView.separated(
-            itemCount: contas.length,
-            itemBuilder: (context, index) {
-              final conta = contas[index];
-              return ContaItem(conta: conta);
-            },
-            separatorBuilder: (context, index) => const Divider(),
-          );
-        },
+      appBar: AppBar(
+        title: const Text('Contas'),
       ),
+      body: FutureBuilder<List<Conta>>(
+          future: contasFuture,
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return const Center(
+                child: CircularProgressIndicator(),
+              );
+            } else if (snapshot.hasError) {
+              return const Center(
+                child: Text("Erro ao carregar contas"),
+              );
+            } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
+              return const Center(
+                child: Text("Nenhuma conta encontrada"),
+              );
+            } else {
+              final contas = snapshot.data!;
+              return ListView.separated(
+                itemCount: contas.length,
+                itemBuilder: (context, index) {
+                  final conta = contas[index];
+                  return ContaItem(conta: conta);
+                },
+                separatorBuilder: (context, index) => const Divider(),
+              );
+            }
+          }),
     );
   }
 }
--- 2-Semestre/expense_tracker_fiap-3SIF/lib/pages/transacoes_page.dart
+++ 2-Semestre/expense_tracker_fiap-3SIF/lib/pages/transacoes_page.dart
@@ -14,78 +14,97 @@ class TransacoesPage extends StatefulWidget {
 
 class _TransacoesPageState extends State<TransacoesPage> {
   final transacoesRepo = TransacoesReepository();
-  late List<Transacao> transacoes;
+  late Future<List<Transacao>> futureTransacoes;
 
   @override
   void initState() {
-    transacoes = transacoesRepo.listarTransacoes();
+    futureTransacoes = transacoesRepo.listarTransacoes();
     super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: const Text('Transações'),
-          actions: [
-            // create a filter menu action
-            PopupMenuButton(
-              itemBuilder: (context) {
-                return [
-                  PopupMenuItem(
-                    child: const Text('Todas'),
-                    onTap: () {
-                      setState(() {
-                        transacoes = transacoesRepo.listarTransacoes();
-                      });
-                    },
-                  ),
-                  PopupMenuItem(
-                    child: const Text('Receitas'),
-                    onTap: () {
-                      setState(() {
-                        transacoes = transacoesRepo
-                            .listarTransacoes()
-                            .where((transacao) =>
-                                transacao.tipoTransacao ==
-                                TipoTransacao.receita)
-                            .toList();
-                      });
-                    },
-                  ),
-                  PopupMenuItem(
-                    child: const Text('Despesas'),
-                    onTap: () {
-                      setState(() {
-                        transacoes = transacoesRepo
-                            .listarTransacoes()
-                            .where((transacao) =>
-                                transacao.tipoTransacao ==
-                                TipoTransacao.despesa)
-                            .toList();
-                      });
-                    },
-                  ),
-                ];
+      appBar: AppBar(
+        title: const Text('Transações'),
+        actions: [
+          // create a filter menu action
+          PopupMenuButton(
+            itemBuilder: (context) {
+              return [
+                PopupMenuItem(
+                  child: const Text('Todas'),
+                  onTap: () {
+                    setState(() {
+                      futureTransacoes = transacoesRepo.listarTransacoes();
+                    });
+                  },
+                ),
+                PopupMenuItem(
+                  child: const Text('Receitas'),
+                  onTap: () {
+                    setState(() {
+                      futureTransacoes = transacoesRepo.listarTransacoes(
+                          tipoTransacao: TipoTransacao.receita);
+                    });
+                  },
+                ),
+                PopupMenuItem(
+                  child: const Text('Despesas'),
+                  onTap: () {
+                    setState(() {
+                      futureTransacoes = transacoesRepo.listarTransacoes(
+                          tipoTransacao: TipoTransacao.despesa);
+                    });
+                  },
+                ),
+              ];
+            },
+          ),
+        ],
+      ),
+      body: FutureBuilder<List<Transacao>>(
+        future: futureTransacoes,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          } else if (snapshot.hasError) {
+            return const Center(
+              child: Text("Erro ao carregar contas"),
+            );
+          } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
+            return const Center(
+              child: Text("Nenhuma conta encontrada"),
+            );
+          } else {
+            final transacoes = snapshot.data!;
+            return ListView.separated(
+              itemCount: transacoes.length,
+              itemBuilder: (context, index) {
+                final transacao = transacoes[index];
+                return TransacaoItem(
+                  transacao: transacao,
+                  onTap: () {
+                    Navigator.pushNamed(context, '/transacao-detalhes',
+                        arguments: transacao);
+                  },
+                );
               },
-            ),
-          ],
-        ),
-        body: ListView.separated(
-          itemCount: transacoes.length,
-          itemBuilder: (context, index) {
-            final transacao = transacoes[index];
-            return TransacaoItem(
-              transacao: transacao,
-              onTap: () {
-                Navigator.pushNamed(context, '/transacao-detalhes',
-                    arguments: transacao);
+              separatorBuilder: (context, index) {
+                return const Divider();
               },
             );
-          },
-          separatorBuilder: (context, index) {
-            return const Divider();
-          },
-        ));
+          }
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.pushNamed(context, "/transacao-cadastro");
+        },
+        child: const Icon(Icons.add),
+      ),
+    );
   }
 }
